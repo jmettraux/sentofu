c1f7034cd112f3e1a42bd0fe2205e2604713a3cd
John Mettraux <jmettraux@gmail.com>
2019-05-17 14:13:15 +0900

    Scaffold api creation

--- lib/sentifi.rb
+++ lib/sentifi.rb
@@ -58,15 +58,40 @@ module Sentifi
 
       OpenStruct.new(YAML.load(File.read(fname)))
     end
+
+    def make_api(spec, name)
+
+      spec['paths'].keys
+    end
+
+    #Dir[File.join(
+    Dir[File.join(__dir__, 'sentifi/sentifi-*.yaml')].each do |fpath|
+
+      spec = YAML.load(File.read(fpath))
+
+      name =
+        spec['info']['title'].split(' - ').last[0..-4].strip
+          .gsub(/([a-z])([A-Z])/) { |_| $1 + '_' + $2.downcase }
+          .gsub(/([A-Z])/) { |c| c.downcase }
+
+      api = Sentifi.send(:make_api, spec, name)
+      define_method(name) { api }
+    end
   end
 end
 
 
 if $0 == __FILE__
 
-  p Time.now
-  t = Sentifi.fetch_token
-  pp t.to_h
-  p [ t.expires_in / 3600 / 24, :days ]
+  #p Time.now
+  #t = Sentifi.fetch_token
+  #pp t.to_h
+  #p [ t.expires_in / 3600 / 24, :days ]
+
+  #y = YAML.load(File.read(File.join(File.dirname(__FILE__), 'sentifi/sentifi-api-docs-sentifi-intelligence_company_api-1.0.0-swagger.yaml')))
+  #pp y['paths'].keys
+
+  pp Sentifi.company
+  #pp Sentifi.markets
 end
 
