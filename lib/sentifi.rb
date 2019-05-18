b26b1808e59750ba23c295cf53fcaba6ff1a9747
John Mettraux <jmettraux@gmail.com>
2019-05-18 10:03:59 +0900

    Make first stab at path tree

--- lib/sentifi.rb
+++ lib/sentifi.rb
@@ -17,35 +17,21 @@ module Sentifi
 
     Sentifi::Api.make(YAML.load(File.read(fpath)))
   end
-
-#  class << self
-#
-#    def make_api(spec, name)
-#
-#      spec['paths'].keys
-#    end
-#
-#    #Dir[File.join(
-#    Dir[File.join(__dir__, 'sentifi/sentifi-*.yaml')].each do |fpath|
-#
-#      spec = YAML.load(File.read(fpath))
-#
-#    end
-#  end
 end
 
 
 if $0 == __FILE__
 
   #p Time.now
-  #t = Sentifi.fetch_token
+  #t = Sentifi::Http.fetch_token
   #pp t.to_h
   #p [ t.expires_in / 3600 / 24, :days ]
 
   #y = YAML.load(File.read(File.join(File.dirname(__FILE__), 'sentifi/sentifi-api-docs-sentifi-intelligence_company_api-1.0.0-swagger.yaml')))
   #pp y['paths'].keys
 
-  #pp Sentifi.company
+  p Sentifi.company.class
+  p Sentifi.company.topic_search
   #pp Sentifi.markets
 end
 
--- lib/sentifi/api.rb
+++ lib/sentifi/api.rb
@@ -1,20 +1,95 @@
 
 module Sentifi
 
-  class Api
+  class Resource
+
+    attr_reader :name
+
+    def initialize(parent, name, point)
+
+      @parent = parent
+      @name = name
+      #@point = point
+    end
+
+    def add_path(path); add(path, nil); end
+    def add_point(path, point); add(path, point); end
+
+    def [](index)
+# TODO
+    end
+
+    protected
+
+    def add(path, point)
+
+      res = Sentifi::Resource.new(self, path.first, point)
+
+      s = (class << self; self; end)
+
+      path0 = path.first.gsub(/-/, '_')
+
+      if point
+        s.define_method(path0) { |key=nil, opts={}|
+          "nada" }
+      else
+        s.define_method(path0) { |key=nil, opts={}|
+          if path[1]
+            res[opts[path[1]]]
+          else
+            res
+          end }
+      end
+
+      res
+    end
+  end
+
+  class Api < Resource
+
+    attr_reader :spec
 
     def initialize(spec, name)
 
-      @spec = spec
-      @name = name
+      super(nil, name, nil)
 
-      p [ name ]
-      spec['paths'].each do |path, value|
-        p path
+      @spec = spec
+
+puts "================== #{name}"
+      spec['paths'].each do |path, point|
+
+p path
+        re = self
+        pas = split_path(path)
+
+        pas[0..-2].each do |pa|
+          re = re.add_path(pa)
+        end
+
+        re.add_point(pas.last, point)
       end
     end
 
     protected
+
+    def split_path(path)
+
+      r = []
+
+      ps = path.split('/')[1..-1]
+      while ps.any?
+        pa = ps.shift
+        pb = ps.first
+        if m = pb && pb.match(/\A\{([^}]+)\}\z/)
+          ps.shift
+          r << [ pa, m[1] ]
+        else
+          r << [ pa ]
+        end
+      end
+
+      r
+    end
 
     class << self
 
